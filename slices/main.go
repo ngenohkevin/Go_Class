e6cc03e19f644612f82490cae96ddf97f2098c51
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-15 11:46:28 +0300

    WTF

--- slices/main.go
+++ slices/main.go
@@ -3,19 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	a := [3]int{1, 2, 3}
+	a := [...]int{1, 2, 3}
 	b := a[0:1]
-
-	fmt.Println("a == ", a)
-	fmt.Println("b == ", b)
-
 	c := b[0:2]
-	fmt.Println("c == ", c)
 
-	fmt.Println(len(b))
-	fmt.Println(cap(b))
+	fmt.Printf("a[%p] = %v\n", &a, a)
+	fmt.Printf("b[%p] = %[1]v\n", b)
 
-	fmt.Println(len(c))
-	fmt.Println(cap(c))
+	c = append(c, 5)
+	fmt.Printf("a[%p] = %v\n", &a, a)
+	fmt.Printf("c[%p] = %[1]v\n", c)
 
 }
