a6b11ba7e05f186aecf84a44d946599288c1580d
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-15 11:17:59 +0300

    slices

--- slices/main.go
+++ slices/main.go
@@ -5,5 +5,8 @@ import "fmt"
 func main() {
 	var s []int
 
+	t := []int{}
+
 	fmt.Printf("%d, %d, %T, %5t, %#[3]v\n", len(s), cap(s), s, s == nil)
+	fmt.Printf("%d, %d, %T, %5t, %#[3]v\n", len(t), cap(t), t, t == nil)
 }
