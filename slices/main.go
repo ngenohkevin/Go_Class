2c89e09a340a2796865f4f59ff44dbfc80459038
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-15 11:54:21 +0300

    c

--- slices/main.go
+++ slices/main.go
@@ -5,13 +5,17 @@ import "fmt"
 func main() {
 	a := [...]int{1, 2, 3}
 	b := a[0:1]
-	c := b[0:2]
+	c := b[0:2:2]
 
 	fmt.Printf("a[%p] = %v\n", &a, a)
 	fmt.Printf("b[%p] = %[1]v\n", b)
+	fmt.Printf("c[%p] = %[1]v\n", c)
 
 	c = append(c, 5)
 	fmt.Printf("a[%p] = %v\n", &a, a)
 	fmt.Printf("c[%p] = %[1]v\n", c)
 
+	c[0] = 9
+	fmt.Printf("a[%p] = %v\n", &a, a)
+	fmt.Printf("c[%p] = %[1]v\n", c)
 }
