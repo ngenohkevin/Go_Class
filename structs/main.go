2473c8c31b2c1351735c23b9f0ee4bb5ff10c279
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-16 12:37:34 +0300

    main

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,19 @@
 package main
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Employee struct {
 	Name   string
 	Number int
 	Boss   *Employee
 	Hired  time.Time
+}
+
+func main() {
+	var e Employee
+
+	fmt.Printf("%T %[1]v\n", e)
 }
