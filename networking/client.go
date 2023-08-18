57d1fa02d2694e786f00fa9efe28b6d6e1aeb21b
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-18 13:11:10 +0300

    Client

---
+++ networking/client.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func client() {
+
+	resp, err := http.Get("http://localhost:8080/" + os.Args[1])
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		body, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(-1)
+		}
+
+		fmt.Println(string(body))
+	}
+
+}
