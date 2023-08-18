4e83c80006af491285fa506a5058f1c8499d5b6f
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-18 12:52:55 +0300

    server

--- networking/main.go
+++ networking/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!, from %s\n", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
