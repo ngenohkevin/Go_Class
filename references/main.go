58d6fc6b2452074eac84c6cabb4f305867d48c2e
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-18 12:27:12 +0300

    value semantics

--- references/main.go
+++ references/main.go
@@ -1,1 +1,15 @@
 package main
+
+import "fmt"
+
+func main() {
+	items := [][2]byte{{1, 2}, {3, 4}, {5, 6}}
+	a := [][]byte{}
+
+	for _, item := range items {
+		a = append(a, item[:])
+	}
+
+	fmt.Println(items)
+	fmt.Println(a)
+}
