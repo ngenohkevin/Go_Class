78bf3a387106af3c5cb0da5257e58f8c13557ace
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-19 10:36:10 +0300

    web server

--- networking/main.go
+++ networking/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -9,13 +8,30 @@ import (
 const url = "https://jsonplaceholder.typicode.com"
 
 type todo struct {
+	UserID    int    `json:"userID"`
 	ID        int    `json:"id"`
 	Title     string `json:"title"`
 	Completed bool   `json:"completed"`
 }
 
+var form = `
+<h1> Todo #{{.ID}} </h1>
+<div> {{printf "User %d" .UserID}} </div>
+<div>{{printf "%s (completed: %t)" .Title .Completed}}</div>
+`
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!, from %s\n", r.URL.Path[1:])
+	const base = "https://jsonplaceholder.typicode.com/"
+
+	resp, err := http.Get(base + r.URL.Path[1:])
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+
+	defer resp.Body.Close()
+
 }
 
 func main() {
