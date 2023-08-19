64d18b4062eb4ee62090018f32d6ef894e31ef4f
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-19 10:22:24 +0300

    copied over

--- networking/main.go
+++ networking/main.go
@@ -6,6 +6,14 @@ import (
 	"net/http"
 )
 
+const url = "https://jsonplaceholder.typicode.com"
+
+type todo struct {
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!, from %s\n", r.URL.Path[1:])
 }
