65041fb642e134b78b9c97d1d6dd88c2d0a9655c
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-21 11:39:04 +0300

    conversion

--- methods_interface/main.go
+++ methods_interface/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"strconv"
+	"strings"
+)
+
+type IntSlice []int
+
+func (is IntSlice) String() string {
+
+	var strs []string
+
+	for _, v := range is {
+		strs = append(strs, strconv.Itoa(v))
+	}
+
+	return "[" + strings.Join
+}
