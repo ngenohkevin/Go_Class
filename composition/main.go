7c0090fdfddc0f403bedfee5aaf3b6de83380ed9
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-22 12:10:39 +0300

    dd

--- composition/main.go
+++ composition/main.go
@@ -2,41 +2,76 @@ package main
 
 import (
 	"fmt"
-	"path/filepath"
 )
 
-type Pair struct {
-	Path string
-	Hash string
+// type Pair struct {
+// 	Path string
+// 	Hash string
+// }
+
+// func (p Pair) String() string {
+// 	return fmt.Sprintf("Hash of %s is %s", p.Path, p.Hash)
+// }
+
+// type PairWithLength struct {
+// 	Pair
+// 	Length int
+// }
+
+// func (p PairWithLength) String() string {
+// 	return fmt.Sprintf("Hash of %s is %s, length %d", p.Path, p.Hash, p.Length)
+// }
+
+// func (p Pair) Filename() string {
+// 	return filepath.Base(p.Path)
+// }
+
+// type Filenamer interface {
+// 	Filename() string
+// }
+
+// func main() {
+// 	p := Pair{"/usr", "Oxfdfe"}
+
+// 	var fn Filenamer = PairWithLength{Pair{"/usr/lib", "Oxdead"}, 133}
+
+// 	fmt.Println(p)
+// 	fmt.Println(fn)
+// 	fmt.Println(fn.Filename())
+// }
+
+type Shape interface {
+	Area() float64
 }
 
-func (p Pair) String() string {
-	return fmt.Sprintf("Hash of %s is %s", p.Path, p.Hash)
+type Circle struct {
+	Radius float64
 }
 
-type PairWithLength struct {
-	Pair
-	Length int
+func (c Circle) Area() float64 {
+	return 3.14 * c.Radius * c.Radius
 }
 
-func (p PairWithLength) String() string {
-	return fmt.Sprintf("Hash of %s is %s, length %d", p.Path, p.Hash, p.Length)
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func (p Pair) Filename() string {
-	return filepath.Base(p.Path)
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
 }
 
-type Filenamer interface {
-	Filename() string
+func PrintArea(s Shape) {
+	fmt.Printf("Area: %f\n", s.Area())
 }
 
 func main() {
-	p := Pair{"/usr", "Oxfdfe"}
+	circle := Circle{Radius: 5}
+	rectangle := Rectangle{Width: 4, Height: 3}
 
-	pl := PairWithLength{Pair{"/usr/lib", "Oxdead"}, 133}
+	PrintArea(circle)    // Prints the area of the circle
+	PrintArea(rectangle) // Prints the area of the rectangle
 
-	fmt.Println(p)
-	fmt.Println(pl)
-	fmt.Println(p.Filename())
+	area := circle.Area() // Calling a method of a concrete type
+	fmt.Println(area)
 }
