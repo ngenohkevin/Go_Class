cfadf556eb17261b711ebf295f382df462796a13
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-22 11:25:08 +0300

    embedding

--- composition/main.go
+++ composition/main.go
@@ -11,6 +11,11 @@ func (p Pair) String() string {
 	return fmt.Sprintf("Hash of %s is %s", p.Path, p.Hash)
 }
 
+type PairWithLength struct {
+	Pair
+	Length int
+}
+
 func main() {
 	p := Pair{"/usr", "Oxfdfe"}
 
