5bc88ab704706249cfdd30fa4e987ad283b9766f
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-23 12:48:17 +0300

    sorting

--- composition/main.go
+++ composition/main.go
@@ -1,42 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"path/filepath"
-)
-
-type Pair struct {
-	Path string
-	Hash string
+type Organ struct {
+	Name   string
+	Weight int
 }
 
-func (p Pair) String() string {
-	return fmt.Sprintf("Hash of %s is %s", p.Path, p.Hash)
-}
-
-type PairWithLength struct {
-	Pair
-	Length int
-}
-
-func (p PairWithLength) String() string {
-	return fmt.Sprintf("Hash of %s is %s, length %d", p.Path, p.Hash, p.Length)
-}
-
-func (p Pair) Filename() string {
-	return filepath.Base(p.Path)
-}
-
-type Filenamer interface {
-	Filename() string
-}
+type Organs []Organ
 
 func main() {
-	p := Pair{"/usr", "Oxfdfe"}
 
-	var fn Filenamer = PairWithLength{Pair{"/usr/lib", "Oxdead"}, 133}
-
-	fmt.Println(p)
-	fmt.Println(fn)
-	fmt.Println(fn.Filename())
 }
