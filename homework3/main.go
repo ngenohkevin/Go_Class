b5dee0b6a73fb48aed69e04c56227aa0c5c23aac
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-30 12:01:02 +0300

    new route

--- homework3/main.go
+++ homework3/main.go
@@ -64,6 +64,18 @@ func (db database) update(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "new price %s for price %s\n", db[item], item)
 }
 
+func (db database) fetch(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+
+	if _, ok := db[item]; !ok {
+		msg := fmt.Sprintf("no such item: %q", item)
+		http.Error(w, msg, http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "item %s has price %s\n", item, db[item])
+}
+
 func main() {
 	db := database{
 		"shoes": 50,
@@ -73,6 +85,7 @@ func main() {
 	http.HandleFunc("/list", db.list)
 	http.HandleFunc("/create", db.add)
 	http.HandleFunc("/update", db.update)
+	http.HandleFunc("/read", db.fetch)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
