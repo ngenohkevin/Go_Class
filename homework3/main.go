4b8bb662096024259cdbd176d2fe5489ca08abe5
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-30 11:32:32 +0300

    fix errors

--- homework3/main.go
+++ homework3/main.go
@@ -12,17 +12,19 @@ func (d dollars) String() string {
 	return fmt.Sprintf("$%.2f", d)
 }
 
+type database map[string]dollars
+
 func (db database) list(w http.ResponseWriter, req *http.Request) {
 
 }
-
-type database map[string]dollars
 
 func main() {
 	db := database{
 		"shoes": 50,
 		"socks": 5,
 	}
+
+	http.HandleFunc("/list", db.list)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
