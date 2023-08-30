e4b3b7d9fee11750f0ab54812ef0955ebac680c1
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-30 11:52:03 +0300

    update

--- homework3/main.go
+++ homework3/main.go
@@ -42,6 +42,27 @@ func (db database) add(w http.ResponseWriter, req *http.Request) {
 
 	fmt.Fprintf(w, "added %s with price %s\n", item, db[item])
 }
+func (db database) Update(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+
+	if _, ok := db[item]; ok {
+		msg := fmt.Sprintf("duplicate item: %q", item)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	p, err := strconv.ParseFloat(price, 32)
+	if err != nil {
+		msg := fmt.Sprintf("Invalid price: %q", price)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	db[item] = dollars(p)
+
+	fmt.Fprintf(w, "added %s with price %s\n", item, db[item])
+}
 
 func main() {
 	db := database{
