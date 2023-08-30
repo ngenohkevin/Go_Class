8a52d2ffed93735f6317bd56263eb3e9fc848fe9
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-30 11:53:04 +0300

    db

--- homework3/main.go
+++ homework3/main.go
@@ -46,7 +46,7 @@ func (db database) Update(w http.ResponseWriter, req *http.Request) {
 	item := req.URL.Query().Get("item")
 	price := req.URL.Query().Get("price")
 
-	if _, ok := db[item]; ok {
+	if _, ok := db[item]; !ok {
 		msg := fmt.Sprintf("duplicate item: %q", item)
 		http.Error(w, msg, http.StatusBadRequest)
 		return
