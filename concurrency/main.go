868efc9ee775b886521e1ccd8884b16a02061a55
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-31 13:02:06 +0300

    comment

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,51 +1,45 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"time"
-)
+// type result struct {
+// 	url     string
+// 	err     error
+// 	latency time.Duration
+// }
 
-type result struct {
-	url     string
-	err     error
-	latency time.Duration
-}
+// func get(url string, ch chan<- result) {
+// 	start := time.Now()
 
-func get(url string, ch chan<- result) {
-	start := time.Now()
+// 	if resp, err := http.Get(url); err != nil {
+// 		ch <- result{url, err, 0}
+// 	} else {
+// 		t := time.Since(start).Round(time.Millisecond)
+// 		ch <- result{url, nil, t}
+// 		resp.Body.Close()
+// 	}
+// }
 
-	if resp, err := http.Get(url); err != nil {
-		ch <- result{url, err, 0}
-	} else {
-		t := time.Since(start).Round(time.Millisecond)
-		ch <- result{url, nil, t}
-		resp.Body.Close()
-	}
-}
+// func main() {
+// 	results := make(chan result)
+// 	list := []string{
+// 		"https://amazon.com",
+// 		"https://www.arnoderrymovers.co.ke",
+// 		"https://google.com",
+// 		"https://github.com",
+// 		"https://nytimes.com",
+// 		"http://wsj.com",
+// 	}
 
-func main() {
-	results := make(chan result)
-	list := []string{
-		"https://amazon.com",
-		"https://www.arnoderrymovers.co.ke",
-		"https://google.com",
-		"https://github.com",
-		"https://nytimes.com",
-		"http://wsj.com",
-	}
+// 	for _, url := range list {
+// 		go get(url, results)
+// 	}
 
-	for _, url := range list {
-		go get(url, results)
-	}
+// 	for range list {
+// 		r := <-results
 
-	for range list {
-		r := <-results
-
-		if r.err != nil {
-			log.Printf("%-20s %s\n", r.url, r.err)
-		} else {
-			log.Printf("%-20s %s\n", r.url, r.latency)
-		}
-	}
-}
+// 		if r.err != nil {
+// 			log.Printf("%-20s %s\n", r.url, r.err)
+// 		} else {
+// 			log.Printf("%-20s %s\n", r.url, r.latency)
+// 		}
+// 	}
+// }
