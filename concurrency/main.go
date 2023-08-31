6103afd0f9f7044aa8fed1683aabf5ab569ea249
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-31 12:37:47 +0300

    results

--- concurrency/main.go
+++ concurrency/main.go
@@ -11,12 +11,23 @@ type result struct {
 	latency time.Duration
 }
 
-func get(url, ch chan<- result) {
+func get(url string, ch chan<- result) {
 	start := time.Now()
 
 	if resp, err := http.Get(url); err != nil {
-
+		ch <- result{url, err, 0}
 	} else {
+		t := time.Since(start).Round(time.Millisecond)
+		ch <- result{url, nil, t}
 		resp.Body.Close()
+	}
+}
+
+func main() {
+	results := make(chan result)
+	list := []string{}
+
+	for _, url := range list {
+		go get(url, results)
 	}
 }
